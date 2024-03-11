ca6133c80fa7fba8ae21fef5ed80a2549e48205d
chungdeptrai2288 <chungdeptrai228@gmail.com>
2024-03-11 11:44:07 +0700

    115

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -8,13 +8,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
 @SpringBootApplication
 public class Demo3Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Demo3Application.class, args);
-		System.out.println("hello");
+		LocalDate localDate = LocalDate.now();
+		LocalTime localTime = LocalTime.now();
+		LocalDateTime localDateTime = LocalDateTime.now();
+		ZonedDateTime zoneDateTime = ZonedDateTime.now();
+		System.out.println("localDate: "+ localDate);
+		System.out.println("localTime: "+ localTime);
+		System.out.println("localDateTime: "+ localDateTime);
+		System.out.println("zoneDateTime: "+ zoneDateTime);
+// giảm đi 1 ngày trong localDate
+		localDate = localDate.minusDays(1);
+		System.out.println("localDate after minus: "+ localDate);
+// công thêm 2 ngày vào localDate
+		localDate = localDate.plusDays(2);
+		System.out.println("localDate after plus: "+ localDate);
+// tạo đối tượng LocalDate từ ngày chỉ rõ
+		LocalDate spec = LocalDate.of(2017, 11, 25);
+		System.out.println("spec: " + spec);
+		long days = ChronoUnit.DAYS.between(localDate, spec);
+		System.out.println("duration between localDate and spec is: " + days + "days");
 	}
 	@Autowired
 	private EmployeeRepository employeeRepository;
